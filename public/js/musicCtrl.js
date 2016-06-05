fe03253b74dd7404e83582a8faf8411972d91232
aynishim <aynishim@yahoo-corp.jp>
2016-06-05 16:16:10 +0900

    説明ページの追加

--- public/js/musicCtrl.js
+++ public/js/musicCtrl.js
@@ -3,16 +3,28 @@ angular.module("novel2music").controller('musicCtrl', function musicCtrl(common,
 
     vm.init = function() {
         vm.scale = {};
-        var url = common.API_HOST + common.API_SCALE;
-        var promise = crudPrvd.get(url);
+        vm.music = {};
+        vm.now_playing = "";
+        var url = [];
+        url.push(common.API_HOST + common.API_SCALE);
+        url.push(common.API_HOST + common.API_MUSIC);
+        var promise = crudPrvd.getArray(url);
         var successCallback = function(response) {
-            vm.scale = response.data.datas;
-            console.dir(vm.scale);
+            vm.scale = response[0].data.datas;
+            vm.music = response[1].data.datas;
+            //vm.now_playing = vm.music[1].file_path;
         };
         var errorCallback = function(response) {
             console.dir(response);
         };
         promise.then(successCallback, errorCallback);
+    };
+
+    vm.selectScale = function(scale_id) {
+        var row = vm.music.filter(function(item, index){
+            if(item.scale_id == scale_id && item.time_id == 1) return true;
+        });
+        vm.now_playing = row[0].file_path;
     };
 
     /*vm.post = function() {
--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -8,4 +8,10 @@ router.get('/explain', function(req, res, next) {
     res.render('questionnaire/explain', { title: 'アンケート概要', user: req.session.user.name });
 });
 
+router.get('/novel/:id', function(req, res, next) {
+    console.log(req.session.user.name);
+    if(req.session.user.name === null) req.session.user.name = "unknown";
+    res.render('questionnaire/novel', { title: '小説', user: req.session.user.name, id: req.params.id });
+});
+
 module.exports = router;
