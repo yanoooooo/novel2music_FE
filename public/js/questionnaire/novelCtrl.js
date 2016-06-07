b84ba9aae186998ec3378753e0371ff73bd104d0
aynishim <aynishim@yahoo-corp.jp>
2016-06-07 16:03:09 +0900

    小説段落と音楽の紐付け部分の実装

--- public/js/questionnaire/novelCtrl.js
+++ public/js/questionnaire/novelCtrl.js
@@ -19,13 +19,52 @@ angular.module("novel2music").controller('novelCtrl', function novelCtrl($scope,
         promise.then(successCallback, errorCallback);
     };
 
-    vm.return = function(number) {
-        vm.paragraph_num--;
+    vm.return = function(is_final) {
+        if(!is_final) {
+            vm.paragraph_num--;
+        }
         SharedStateService.datas.pop();
     };
 
-    vm.post = function() {
-        
+    vm.post = function(user) {
+        var url = common.API_HOST + common.API_USER +"?name=" + user;
+        var promise = crudPrvd.get(url);
+        var successCallback = function(response) {
+            var user_data = response.data.datas[0];
+            //formatted data
+            vm.datas = [];
+            vm.datas = SharedStateService.datas;
+            for(var i in vm.datas) {
+                vm.datas[i].user_id = user_data.id;
+            }
+            vm.comebackPost(vm.datas, 0);
+        };
+        var errorCallback = function(response) {
+            var sentence = "ユーザ情報の取得に失敗しました";
+            resultPrvd.showFaild(vm, sentence);
+        };
+        promise.then(successCallback, errorCallback);
+    };
+
+    vm.comebackPost = function(data, num) {
+        console.dir(data);
+        var url = common.API_HOST + common.API_RELATION_NOVEL;
+        var promise = crudPrvd.post(url, data[num]);
+        var successCallback = function(response) {
+            num++;
+            if(num < data.length) {
+                
+                vm.comebackPost(data, num);
+            } else {
+                var sentence = "回答の登録が完了しました";
+                resultPrvd.showResult(vm, sentence, '/');
+            }
+        };
+        var errorCallback = function(response) {
+            var sentence = "回答の登録に失敗しました";
+            resultPrvd.showFaild(vm, sentence);
+        };
+        promise.then(successCallback, errorCallback);
     };
 
     $scope.$watch(function() {
